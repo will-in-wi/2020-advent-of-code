4f93f2ae0c8d19bfda492b3020cf371cfc2a9245
William Johnston <william.johnston@drip.com>
2020-12-01 10:04:34 -0600

    Switch to readlines due to a pointer from @keheitz

--- day-1/main.rb
+++ day-1/main.rb
@@ -3,7 +3,7 @@
 # Part 1 used 2. Part 2 used 3.
 RESULT_COUNT = 3
 
-entries = File.read('input.txt').split("\n").map(&:to_i)
+entries = File.readlines('input.txt').map(&:to_i)
 
 entries.permutation(RESULT_COUNT) do |nums|
   next unless nums.sum == 2020
